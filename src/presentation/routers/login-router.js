b846b27e1c2e915edd96dcea49b9cfdf0211bca2
Rafael Melo <mellowteam@hotmail.com>
2022-02-09 23:38:34 -0300

    removing comentary

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -21,6 +21,7 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ accessToken })
     } catch (error) {
+      // console.log(error)
       return HttpResponse.serverError()
     }
   }

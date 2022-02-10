6c16b42ba2ba84d17d22f6a9d88b75f802ca0756
Rafael Melo <mellowteam@hotmail.com>
2022-02-10 20:01:24 -0300

    modify HttpResponse.badRequest to receive error

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,5 +1,6 @@
 import HttpResponse from '../helpers/http-response'
 import 'babel-polyfill'
+import MissingParamError from '../helpers/missing-param-error'
 
 module.exports = class LoginRouter {
   constructor (authUseCase) {
@@ -10,10 +11,10 @@ module.exports = class LoginRouter {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return HttpResponse.badRequest('email')
+        return HttpResponse.badRequest(new MissingParamError('email'))
       }
       if (!password) {
-        return HttpResponse.badRequest('password')
+        return HttpResponse.badRequest(new MissingParamError('password'))
       }
       const accessToken = await this.authUseCase.auth(email, password)
       if (!accessToken) {

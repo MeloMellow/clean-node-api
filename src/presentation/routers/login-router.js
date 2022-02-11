4115e8d1b06b9cddf9d038151d92d758a68c56c2
Rafael Melo <mellowteam@hotmail.com>
2022-02-10 21:44:29 -0300

    add jest configurations

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -27,6 +27,7 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ accessToken })
     } catch (error) {
+      console.log(error)
       return HttpResponse.serverError()
     }
   }

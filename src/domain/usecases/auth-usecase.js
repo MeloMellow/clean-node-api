7eafff6fbf8021d7c7cbf6beb2140768dbbdcafc
Rafael Melo <mellowteam@hotmail.com>
2022-02-18 13:27:08 -0300

    ensure AuthUseCase throws if no dependecy is provided

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,7 +1,7 @@
 import { MissingParamError } from '../../utils/errors'
 
 module.exports = class AuthUseCase {
-  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator }) {
+  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator } = {}) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
     this.tokenGenerator = tokenGenerator

2810f0a228c9f7e6787f2fec8668486c5dbfca44
Rafael Melo <mellowteam@hotmail.com>
2022-02-24 23:08:48 -0300

    refactor + Ensure Encrypter returns true if bcrypt returns true

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill'
+
+class Encrypter {
+  async compare (password, hashedPassword) {
+    return true
+  }
+}
+
+describe('Encrypter', () => {
+  test('SHould return true if bcrpyt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_password', 'hashed_password')
+    expect(isValid).toBe(true)
+  })
+})

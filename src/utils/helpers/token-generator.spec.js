817569cbbc53ca57bb4b86919c3a0130ad63eb3e
Rafael Melo <mellowteam@hotmail.com>
2022-03-07 14:21:50 -0300

    ensure TokenGenerator returns a token if jwt returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,15 +1,23 @@
 import 'babel-polyfill'
+import jwt from 'jsonwebtoken'
 
 class TokenGenerator {
-  async generate () {
-    return null
+  async generate (id) {
+    return jwt.sign(id, 'secrect')
   }
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })

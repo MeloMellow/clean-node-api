279806b634d0620f91ead66962ee495f3fb35160
Rafael Melo <mellowteam@hotmail.com>
2022-03-07 14:57:03 -0300

    move TokenGenerator to its own file

---
+++ src/utils/helpers/token-generator.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import jwt from 'jsonwebtoken'
+import MissingParamError from '../errors/missing-param-error'
+
+module.exports = class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+    return jwt.sign(id, this.secret)
+  }
+}

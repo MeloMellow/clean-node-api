5fa808b24408bcdc8cc2636cb4cb43fce02199f2
Rafael Melo <mellowteam@hotmail.com>
2022-03-17 17:50:53 -0300

    UpdateAccessTokenRepository moved to his own file

---
+++ src/infra/repositories/update-access-token-repository.js
@@ -0,0 +1,24 @@
+import MissingParamError from '../../utils/errors/missing-param-error'
+import 'babel-polyfill'
+
+module.exports = class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, accessToken) {
+    if (!userId) {
+      throw new MissingParamError('userId')
+    }
+    if (!accessToken) {
+      throw new MissingParamError('accessToken')
+    }
+    await this.userModel.updateOne({
+      _id: userId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+  }
+}

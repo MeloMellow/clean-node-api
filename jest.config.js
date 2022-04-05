22cd4c5041cfeb72e4475c12570568a2f54122b0
Rafael Melo <mellowteam@hotmail.com>
2022-04-04 22:51:38 -0300

    ignore main module in coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 
   preset: '@shelf/jest-mongodb',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   watchPathIgnorePatterns: ['globalConfig']
 
 }
